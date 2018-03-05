dd94bb8604c6adf8411b6e22630027ae315d78d4
Amgalan <amgalan@github.com>
2018-03-05 23:31:15 +0300

    Описания

---
+++ barselon.py
@@ -0,0 +1,26 @@
+import json
+
+
+def load_data(filepath):
+    with open(filepath, 'r', encoding="Latin-1") as file_handler:
+        data1 = json.load(file_handler)
+        return data1
+
+from barselon import load_data
+
+def get_biggest_bar(data):
+    data=load_data("bar.txt")
+    print(data["features"][])
+
+
+
+def get_smallest_bar(data):
+    pass
+
+
+def get_closest_bar(data, longitude, latitude):
+    pass
+
+
+if __name__ == '__main__':
+    print(get_biggest_bar(load_data("bar.txt")))
